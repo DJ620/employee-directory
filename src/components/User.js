da850e2c9c2a634b0f698387a3ec3df1b056821c
DJ Hersh <DanielJHersh@gmail.com>
2021-01-12 19:08:04 -0800

    Added a styles page to make the cursor a pointer when hovering over 'name' or 'location'

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,6 @@
 import React from "react";
 import API from "../utils/API";
+import "../styles/style.css";
 
 class User extends React.Component {
     state = {
@@ -19,18 +20,14 @@ class User extends React.Component {
     };
 
     sortUsers = event => {
-        switch (event.currentTarget.attributes.getNamedItem("data-value").value) {
-            case "name":
-                this.setState({
-                    users: this.state.users.sort((a,b) => a.name.last.localeCompare(b.name.last))
-                });
-                break;
-            case "state":
-                this.setState({
-                    users: this.state.users.sort((a,b) => a.location.state.localeCompare(b.location.state))
-                })
-            default:
-                return;
+        if (event.currentTarget.attributes.getNamedItem("data-value").value === "name") {
+            this.setState({
+                users: this.state.users.sort((a,b) => a.name.last.localeCompare(b.name.last))
+            });
+        } else {
+            this.setState({
+                users: this.state.users.sort((a,b) => a.location.state.localeCompare(b.location.state))
+            });
         };
     };
 
@@ -41,10 +38,10 @@ class User extends React.Component {
                     <thead>
                         <tr>
                             <th className="text-center" scope="col">Picture</th>
-                            <th className="text-center" scope="col" data-value="name" onClick={this.sortUsers}>Name</th>
+                            <th className="text-center hover" scope="col" data-value="name" onClick={this.sortUsers}>Name</th>
                             <th className="text-center" scope="col">Email Address</th>
                             <th className="text-center" scope="col">Phone Number</th>
-                            <th className="text-center" scope="col" data-value="state" onClick={this.sortUsers}>Location</th>
+                            <th className="text-center hover" scope="col" data-value="state" onClick={this.sortUsers}>Location</th>
                         </tr>
                     </thead>
                     <tbody>
